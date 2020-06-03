d7387fda7184bc4776175c21bdae235bd6191016
hutf-51100726 <hutf_51100726@163.com>
2020-06-03 15:28:14 +0900

    项目初始化

---
+++ idea-git-test/src/Test01.java
@@ -0,0 +1,12 @@
+/**
+ * @author hutengfei
+ * @version V1.0
+ * @Package PACKAGE_NAME
+ * @date 2020/6/3 15:22
+ * @Copyright © 株式会社多言语系统研究所
+ */
+public class Test01 {
+    public static void main(String[] args) {
+        System.out.println("测试.......");
+    }
+}
